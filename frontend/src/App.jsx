c7842d2762d77ac1e4b5d13dcb7987254d2968d8
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-04-08 22:20:29 -0300

    Finished Header and theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header"
 
 export default function App() {
+    const browserTheme = () => {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    };
+
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem('theme') || browserTheme();
+    });
+
+    useEffect(() => {
+
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        };
+
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
     return (
-        <Header />
+        <Header setTheme={setTheme} theme={theme} />
     )
 };
