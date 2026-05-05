4b194e1e78a7416e4f26290d9df943e25534b703
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-05-05 19:44:09 -0300

    GridTitle and scroll fix

--- frontend/src/components/GridTitle.jsx
+++ frontend/src/components/GridTitle.jsx
@@ -9,7 +9,7 @@ export default function GridTitle({ name, active, setActive }) {
             <button onClick={() => setActive(!active)} className="flex group peer gap-1 sm:gap-3 items-center justify-center font-semibold font-[Inter]">
                 <div className="title-bar relative bg-primary w-1 h-8 rounded-full" />
                 <ArrowRightIcon className={`${active ? "rotate-90 opacity-100" : "rotate-0 sm:opacity-0"} h-7.5 xl:h-8.5 stroke-[2.5] font-extrabold opacity-0 group-hover:flex group-hover:opacity-100 hover:bg-hover-bg active:scale-95 active:bg-active-bg rounded-full aspect-square p-1 transition-all duration-200`} />
-                <h2 className={`${active ? "sm:translate-x-0 xl:translate-x-0" : "-translate-x-4 sm:-translate-x-7"} text-4xl group-hover:scale-110 xl:group-hover:scale-100 xl:text-5xl xl:-translate-x-9 group-hover:translate-x-0 font-semibold active:scale-90 xl:active:scale-95 font-[satoshi] transition-all duration-200`}>{name}</h2>
+                <h2 className={`${active ? "sm:translate-x-0 xl:translate-x-0" : "-translate-x-4 sm:-translate-x-7"} text-4xl group-hover:scale-110 xl:group-hover:scale-100 xl:text-5xl xl:-translate-x-9 group-hover:translate-x-0 font-semibold active:scale-90 xl:active:scale-95 font-[satoshi] transition-all duration-200 z-3`}>{name}</h2>
             </button>
             <h3 className="text-text-soft pl-4 text-sm sm:text-base sm:max-w-1/2 md:max-w-1/3 opacity-75 peer-hover:opacity-100 transition-opacity duration-200">{descText()}</h3>
         </div>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header({ theme, setTheme, isMenuOpen, setMenuOpen }) {
     return (
         <header className="bg-header py-6 flex justify-center items-center relative transition-colors duration-200">
             <HamMenu isMenuOpen={isMenuOpen} setMenuOpen={setMenuOpen} />
-            <nav aria-label="Main Navigation Menu" className="absolute hidden sm:block sm:left-5 md:left-10 lg:left-12">
+            <nav aria-label="Main Navigation Menu" className="absolute hidden md:block sm:left-5 md:left-10 lg:left-12">
                 <ul className="flex gap-6 lg:gap-8 xl:gap-12 text-text font-[Inter] font-medium text-lg lg:text-xl">
                     <li className="hover:-translate-y-1 transition-transform duration-200"><a className="hover:cursor-pointer opacity-90 hover:opacity-100 transition-opacity duration-200">Lições</a></li>
                     <li className="hover:-translate-y-1 transition-transform duration-200"><a className="hover:cursor-pointer opacity-90 hover:opacity-100 transition-opacity duration-200">Calendário</a></li>
